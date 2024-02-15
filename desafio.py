01649a1f3ae22e16bfbe0d5b2133a7702cb98433
lucaslmeireles <lluciomeireles@gmail.com>
2024-02-15 20:09:00 -0300

    ok

--- desafio.py
+++ desafio.py
@@ -16,10 +16,10 @@ class Sheets():
         self.values = None
         self.SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
         self.SPREADSHEET_ID = None
-        self.RANGE_NAME = "engenharia_de_software!B4:H28"
+        self.RANGE_NAME = None
     #Define the methods to initialize the googlespreadsheet API
     def build(self):
-        #Code avaialable in the googlespreadsheet API documentation in https://developers.google.com/sheets/api/quickstart/python?hl=pt-br
+        #Code available in the googlespreadsheet API documentation in https://developers.google.com/sheets/api/quickstart/python?hl=pt-br
         creds = None
         if os.path.exists("token.json"):
             creds = Credentials.from_authorized_user_file("token.json",self.SCOPES)
@@ -34,7 +34,7 @@ class Sheets():
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
         try:
-            #With the authentication process is done, then the service is initialized
+            #With the authentication process is done, then the service will be initialized
             self.service = build("sheets", "v4", credentials=creds)
         except Exception as e:
             print(e)
@@ -61,7 +61,7 @@ class Sheets():
             return False
     
     #Function that update the state of the student, receiving the range and the value to be put in each student
-    def updateState(self,range, value):
+    def update_state(self,range, value):
         try:
             result = (
                  self.service.spreadsheets()
@@ -74,8 +74,8 @@ class Sheets():
             print("Error")
             return False
     
-    #Function that update the minimum value to be approved in the final exam, for any other stundent the default value is 0
-    def naf(self, range, value={"values": [[0]]}):
+    #Function that update the minimum value to be approved in the final exam, for any other student the default value is 0
+    def final_note(self, range, value={"values": [[0]]}):
         try:
             result = (
                  self.service.spreadsheets()
@@ -89,47 +89,47 @@ class Sheets():
             return False
     
     #Helper function to get the index of the row
-    def helperIndex(self,row):
+    def helper_index(self,row):
         return self.values.index(row) + 4
     
     #Function that iterate over the values of the spreadsheet, it calculates the avarage and the percentage of the classes attended
-    #Then it updates the state of the student with a if and else condition
+    #Then it updates the state of the student  with a if and else condition
     def iterate(self):
         for row in self.values:
             #Define the values to be checked
             #For a personal choice I preferred to divide by 30 to get the scale of the avarege to 0-10
             avg = (int(row[2]) + int(row[3]) + int(row[4])) / 30
-            percentAttendClasses = (60 - int(row[1])) / 60 * 100
+            attendance_percentage = (60 - int(row[1])) / 60 * 100
             range = self.helperIndex(row)
-            print(f"Name: {row[0]}, Avarage: {round(avg,2)}, Frequency: {round(percentAttendClasses,2)}%")
-            if percentAttendClasses < 75:
+            print(f"Name: {row[0]}, Avarage: {round(avg,2)}, Frequency: {round(attendance_percentage,2)}%")
+            if attendance_percentage < 75:
                 print(f"The stundent {row[0]} was disapproved by absence")
-                #The updateState function is called and passed the range and the value to be updated
-                self.updateState(range,{"values": [['Reprovado por falta']]})
-                self.naf(range)
+                #The update_state function is called and passed the range and the value to be updated
+                self.update_state(range,{"values": [['Reprovado por falta']]})
+                self.final_note(range)
             elif (avg < 5):
                 print(f"The student {row[0]} was reproved by avarage")
-                self.updateState(range,{"values": [['Reprovado por média']]})
-                self.naf(range)
+                self.update_state(range,{"values": [['Reprovado por média']]})
+                self.final_note(range)
             elif (avg >= 5 and avg < 7):
-                #In this situation besides the "Situação" column be updated the "Nota para aprovação final" column is updated as well
+                #In this situation besides the "Situação" column be updated, the "Nota para aprovação final" column is updated as well
                 # With the formula bellow
                 print(f"The student {row[0]} is in the final exam")
-                self.updateState(range,{"values": [['Exame final']]})
-                #The formula calculates how much the students needs to pass the final exam
-                self.naf(range,{"values": [[math.ceil(avg*10 - (2 * 5))]]})
+                self.update_state(range,{"values": [['Exame final']]})
+                #The formula calculates how much the student needs to pass the final exam
+                self.final_note(range,{"values": [[math.ceil(avg*10 - (2 * 5))]]})
             else:
                 #If the student get any avarage above 7 he is approved	
                 print(f"The student {row[0]} was approved")  
-                self.updateState(range,{"values": [['Aprovado']]})
-                self.naf(range)
+                self.update_state(range,{"values": [['Aprovado']]})
+                self.final_note(range)
     #Method that executes the whole process
     def run(self, spreadsheetId, range):
-        print("Iniciando...")
+        print("Running...")
         self.build()
         self.get(spreadsheetId, range)
         self.iterate()
-        print("Fim")
+        print("End of program")
 
 
 #Instantiate the class and call the run method
